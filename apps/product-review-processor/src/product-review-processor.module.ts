420dfa7edde8ce27f60c60e89536e803d8d1ceb0
Jakub Stibůrek <jakub@stiburek.dev>
2025-08-30 14:28:11 +0200

    fix(processor): database connection

--- apps/product-review-processor/src/product-review-processor.module.ts
+++ apps/product-review-processor/src/product-review-processor.module.ts
@@ -6,12 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: process.env.MONGO_INITDB_HOST || 'localhost',
+      host: process.env.MONGO_INITDB_HOST || 'mongodb',
       port: parseInt(process.env.MONGO_INITDB_PORT || '27017'),
       username: process.env.MONGO_INITDB_ROOT_USERNAME || 'admin',
       password: process.env.MONGO_INITDB_ROOT_PASSWORD || 'password',
       database:
         process.env.MONGO_INITDB_DATABASE || 'product_review_aggregate_db',
+      authSource: 'admin',
       entities: [],
       synchronize: true,
     }),
