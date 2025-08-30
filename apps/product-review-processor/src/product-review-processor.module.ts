50186fe2156a2910d44376624b0f76b07c3cac80
Jakub Stibůrek <jakub@stiburek.dev>
2025-08-30 13:43:48 +0200

    refactor(processor): remove boilerplate

--- apps/product-review-processor/src/product-review-processor.module.ts
+++ apps/product-review-processor/src/product-review-processor.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductReviewProcessorController } from './product-review-processor.controller';
-import { ProductReviewProcessorService } from './product-review-processor.service';
 
 @Module({
   imports: [],
   controllers: [ProductReviewProcessorController],
-  providers: [ProductReviewProcessorService],
 })
-export class ProductReviewProcessorModule {}
+export class ProductReviewProcessorModule { }
--- apps/product-review-processor/src/product-review-processor.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ProductReviewProcessorService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
