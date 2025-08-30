be33da8e5bb7bea4ae507d3440c3e59adb5d0217
Jakub Stibůrek <jakub@stiburek.dev>
2025-08-30 15:36:41 +0200

    feat(product): set up OpenAPI documentation

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './product-service.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Product System')
+    .setDescription('API for manipulating products and reviews')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Product Service running on port: ${port}`);
