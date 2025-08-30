53e03f9994bdb63cc190ad93c4e8c95d7eccc43c
Jakub Stibůrek <jakub@stiburek.dev>
2025-08-30 17:02:33 +0200

    feat(product): create product response documentation

--- apps/product-service/src/product-service.controller.ts
+++ apps/product-service/src/product-service.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, Body, Inject } from '@nestjs/common';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { CreateProductUseCase } from './application/create-product.use-case';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateProductResponseDto } from './dtos/create-product-response.dto';
 
 @Controller('api/v1')
 export class AppController {
@@ -10,6 +11,11 @@ export class AppController {
   @Post('products')
   @ApiOperation({
     summary: 'Creates new product',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: CreateProductResponseDto,
   })
   async createProduct(@Body() dto: CreateProductDto) {
     return this.createProductUseCase.execute(
