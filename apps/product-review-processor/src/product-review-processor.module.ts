8dfe5495547a48ec792b0d8f896853fceca59f72
Jakub Stibůrek <jakub@stiburek.dev>
2025-08-30 14:23:24 +0200

    feat: set up database connections

--- apps/product-review-processor/src/product-review-processor.module.ts
+++ apps/product-review-processor/src/product-review-processor.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductReviewProcessorController } from './product-review-processor.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: process.env.MONGO_INITDB_HOST || 'localhost',
+      port: parseInt(process.env.MONGO_INITDB_PORT || '27017'),
+      username: process.env.MONGO_INITDB_ROOT_USERNAME || 'admin',
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD || 'password',
+      database:
+        process.env.MONGO_INITDB_DATABASE || 'product_review_aggregate_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [ProductReviewProcessorController],
 })
 export class ProductReviewProcessorModule { }
--- apps/product-service/src/product-service.module.ts
+++ apps/product-service/src/product-service.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './product-service.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -19,6 +20,16 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         },
       },
     ]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      username: process.env.POSTGRES_USER || 'admin',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.POSTGRES_DB || 'product_db',
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
 })
